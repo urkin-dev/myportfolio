b3b786195b43072190af6604a128b4d6552a20ec
Ivan <yurkin.ivan5456@gmail.com>
2020-08-26 17:21:02 +0500

    Add svgs and vue-fullpage

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div id="app">
+        <div class="tree">
+        </div>
+        
+    </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+    name: "App"
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+    padding: 0;
+    margin: 0;
+    background-image: url("./assets/img/back.svg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: 0 -30px;
+
+    .tree {
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        max-width: 55%;
+        width: 55%;
+        height: 100vh;
+        background-image: url("./assets/img/tree.svg");
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
 }
 </style>
